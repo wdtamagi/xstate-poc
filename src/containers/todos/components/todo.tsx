1f1b4d6dc0fb8c26c52600373ab953aa6ce29f3b
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-04-18 23:26:18 -0300

    Added Todo component.

---
+++ src/containers/todos/components/todo.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const checkClasses = "fill-current text-gray-500 m-auto h-5 w-5";
+const UnChecked = () => {
+  return (
+    <svg className={checkClasses} stroke-width="0" viewBox="0 0 24 24">
+      <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path>
+    </svg>
+  );
+};
+const Checked = () => {
+  return (
+    <svg className={checkClasses} stroke-width="0" viewBox="0 0 24 24">
+      <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+    </svg>
+  );
+};
+
+interface ITodoProps {
+  /** The text inside the button */
+  text: string;
+  /** The checked state */
+  checked: boolean;
+  /** The handler to control checked state */
+  onClick: () => void;
+}
+
+export const Todo = ({ text, checked, onClick }: ITodoProps) => {
+  return (
+    <li
+      className="flex flex-row text-gray-600 bg-gray-200 px-5 py-2"
+      style={{ borderTop: "1px solid #bdc2ca5e" }}
+      onClick={onClick}
+    >
+      {checked ? <Checked /> : <UnChecked />}
+      <span
+        className={`w-full truncate text-gray-600 border border-gray-200 py-1 pl-4 ${
+          checked && "line-through"
+        }`}
+      >
+        {text}
+      </span>
+    </li>
+  );
+};
