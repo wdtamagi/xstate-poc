b76d1f4f935a4587daf0c59eb5f80e33c5bda12d
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-04-18 23:27:58 -0300

    Adjusted main App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Todos } from "./containers/todos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-6">
+      <h1 className="mx-auto text-gray-600 mt-2 mb-4 text-center">TODOS</h1>
+      <Todos />
     </div>
   );
 }
