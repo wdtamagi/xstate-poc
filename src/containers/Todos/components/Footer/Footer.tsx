6b0d000e1aa645f155962655fcc924a895cc49f6
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-05-01 21:42:10 -0300

    fix: Fixed responsive for footer component

--- src/containers/Todos/components/Footer/Footer.tsx
+++ src/containers/Todos/components/Footer/Footer.tsx
@@ -1,33 +1,34 @@
 import React from "react";
-
-interface IFooterProps {
-  /** The remainings todos*/
-  remaining: number;
-  /** The active view */
-  activeView: string;
-  /** The handler to control active view */
-  onChangeView: (view: string) => void;
-  /** The control for clear button */
-  anyTodoDone: boolean;
-}
+import { IProps } from "./types";
 
 const Footer = ({
   remaining,
   activeView,
   onChangeView,
   anyTodoDone,
-}: IFooterProps) => {
+}: IProps) => {
   const getButtonClass = (active: boolean = false) =>
-    `hover:bg-gray-400 font-semibold hover:text-white py-2 px-4 rounded ${
+    `hover:bg-gray-400 font-semibold hover:text-white py-2 px-4 text-xs rounded ${
       active ? "bg-gray-400 text-white" : "bg-transparent text-gray-600"
     }`;
 
   return (
-    <div className="relative flex flex-row justify-between items-center px-5 py-4 bg-gray-200 text-gray-600 text-xs">
-      <span className="py-2">{`${remaining} ${
-        remaining > 1 ? "items" : "item"
-      } left`}</span>
-      <div className="absolute h-full inset-0 m-0 p-0 text-center px-5 py-4">
+    <div className="relative flex flex-col px-5 py-4 bg-gray-200">
+      <div className="flex flex-row justify-between items-center text-gray-600 text-xs w-full pb-2 sm:pb-0">
+        <span className="py-2">{`${remaining} left`}</span>
+        {anyTodoDone && (
+          <button
+            style={{ outline: "none" }}
+            className={`${getButtonClass()} z-10`}
+          >
+            Clear completed
+          </button>
+        )}
+      </div>
+      <div
+        className="pt-4 text-center w-full sm:absolute sm:h-full sm:inset-0 sm:px-5 sm:py-4"
+        style={{ borderTop: "1px solid #bdc2ca5e" }}
+      >
         <button
           style={{ outline: "none" }}
           className={getButtonClass(activeView === "all")}
@@ -50,14 +51,6 @@ const Footer = ({
           Completed
         </button>
       </div>
-      {anyTodoDone && (
-        <button
-          style={{ outline: "none" }}
-          className={`${getButtonClass()} z-10`}
-        >
-          Clear completed
-        </button>
-      )}
     </div>
   );
 };
